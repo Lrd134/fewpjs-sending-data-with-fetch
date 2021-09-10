86d45ac45c94f77b420e7d12dc45ce0eb9a4a57f
Lrd134 <larryc3200@gmail.com>
2021-09-10 15:41:09 -0400

    commit tree af8f31cff7dd3c3b9ac9a09722002b398dea6649

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-function submitData( name, email ) {
-  return fetch( 'http://localhost:3000/users', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( {
-        name,
-        email
-      } )
-    } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
-      document.body.innerHTML = error.message
-    } )
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  }).then(resp => resp.json()).then(obj => {
+    document.body.innerHTML = obj.id;
+  }).catch(error => {
+    console.log(error.message);
+    document.body.innerHTML = error.message;
+  });
+
 }
